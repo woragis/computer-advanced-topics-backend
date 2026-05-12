d97a27066b3475a0d3a2e096ac15672e060e65ba
woragis <masteringthecode.woragis+github@gmail.com>
2026-05-12 19:28:13 -0300

    Delete Java files

--- src/main/java/com/fakeradar/FakeRadarApplication.java
+++
@@ -1,5 +0,0 @@
-package com.fakeradar;
-
-public class FakeRadarApplication {
-
-}
--- src/main/java/com/fakeradar/repository/ClaimRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fakeradar.repository;
-
-import com.fakeradar.model.Claim;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ClaimRepository extends JpaRepository<Claim, UUID> {
-}
